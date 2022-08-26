c07d8d1a89c61dd3869ce3c9fc34d4cd879e149b
Seungjee Lee <moondgod@gmail.com>
2022-08-26 12:03:21 +0900

    someModify

---
+++ JS/link.js
@@ -0,0 +1,10 @@
+const loginBTN = document.querySelector(".user-btn .sign-in");
+const signupBTN = document.querySelector(".user-btn .sign-up");
+
+loginBTN.addEventListener("click",() => {
+    location = "../PageHTML/Login.html";
+} );
+
+signupBTN.addEventListener("click",() => {
+    location= "../sign_up/img/page/moment.html"
+})
